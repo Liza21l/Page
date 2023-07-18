b8c173d128b95c5f14661f490e7fc40297f9280c
Liza21l <Liza2009koshenya@gmail.com>
2023-07-18 11:12:20 +0300

    page

--- script.js
+++ script.js
@@ -5,6 +5,48 @@ const formEl = {
     price: document.querySelector('#formPrice'),
     btnCreate: document.querySelector('#formBtn')
 }
+const productsEl = document.querySelector('#products')
+
+let products = []
+
+
+const renderProducts = () => { 
+    productsEl.innerHTML = ""
+    products.forEach(productItem => { 
+        productsEl.innerHTML += `
+        <div product-id="${productItem.id}" class="products_item">
+            <p class="title">${productItem.name}</p>
+            <p>${productItem.description}</p>
+            <p>Quantity ${productItem.quantity}</p>
+            <p class="price">${productItem.price} грн</p>
+            <button product-id="${productItem.id}" class="btnDelete">delete</button>
+        </div>
+        `
+    })
+    const btnsDelete = document.querySelectorAll(".btnDelete")
+    btnsDelete.forEach(btnItem => {
+        btnItem.addEventListener("click", () => {
+            const productId = btnItem.getAttribute("product-id")
+            axios.delete(`http://localhost:4000/products/delete?id=${productId}`)
+                .then((res) => {
+                    console.log(res)
+                    getProducts()
+                })
+        })
+    })
+}
+
+const getProducts = () => {
+    axios.get("http://localhost:4000/products/get-all")
+        .then((res) => {
+            products = [
+                ...res.data
+            ]
+            renderProducts()
+        })
+}
+getProducts()
+
 formEl.btnCreate.addEventListener('click', () => {
     const formData = {
         name: formEl.name.value,
@@ -14,6 +56,9 @@ formEl.btnCreate.addEventListener('click', () => {
     }
     axios.post('http://localhost:4000/products/create',{...formData})
         .then(res=> {
-            console.log(res)
+            console.log(res.data)
+            getProducts()
         })
 })
+
+
